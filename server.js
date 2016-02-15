87d86a3b6298aedd985853e610c1e98d95c54e3c
Sachin Mour <rtr.sachinmour@gmail.com>
2016-02-15 17:22:42 +0000

    Added local authentication to the app

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 'use strict';
-
 require('dotenv').config();
 
-process.env.hist = new Array(10);
-
 var express = require('express'),
-    routes = require('./app/routes/index.js');
-    
-var app = express();
+    routes = require('./app/routes/index.js'),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    flash = require("connect-flash"),
+    morgan = require("morgan"),
+    cookieParser = require("cookie-parser"),
+    bodyParser = require("body-parser"),
+    session = require("express-session");
 
-routes(app);
+var app = express();
+mongoose.connect(process.env.MONGOLAB_URI);
+
+require('./config/passport')(passport);
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json());             //
+app.use(bodyParser.urlencoded({         // get information from html forms    
+  extended: true                        //
+}));
+
+app.set('view engine', 'jade'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch', saveUninitialized: true, resave: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+    
+routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
