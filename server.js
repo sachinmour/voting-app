4e5178a3e0803f427af1189f7754ce1571b7074c
Sachin Mour <rtr.sachinmour@gmail.com>
2016-03-19 17:54:06 +0000

    Testing chart.js

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ app.use(session({ secret: 'ilovescotchscotchyscotchscotch', saveUninitialized: t
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-    
+
+app.use('/assets', express.static(process.cwd() + '/views/assets'));
+
 routes(app, passport);
 
 var port = process.env.PORT || 8080;
